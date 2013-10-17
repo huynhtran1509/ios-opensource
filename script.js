e9a8a31b0fa7eb22ef4d19b2b49f08b7f7a22392
Andrew Carter <andrew.r.carter@gmail.com>
2013-10-17 13:09:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var repos = [
+				"WTAScrollViewKeyboardManager",
+				"WTACURLStringFromURLRequest",
+  				"WTATagStringBuilder",
+   				"MTStackViewController",
+   				"uicolor-helpers",
+   			].sort();
+var repoTemplate;
+
+$(document).ready(function() {
+	compileTemplates();
+	displayPlaceholder();
+	loadRepos();
+});
+
+function loadRepos() {
+	repos.forEach(function (value, index) {
+		getRepo(value, function(data) {
+			$("#repos li:eq(" + index + ")").replaceWith(repoTemplate(data));
+		});
+	});
+}
+
+function displayPlaceholder() {
+	repos.forEach(function (value) {
+		var context = {name: value};
+		$("#repos").append(repoTemplate(context));
+	});
+}
+
+function compileTemplates() {
+	repoTemplate = Handlebars.compile($("#repo-template").html());
+}
+
+function getRepo(repoName, completion) {
+	$.getJSON("https://api.github.com/repos/willowtreeapps/" + repoName, function(data) {
+ 		completion(data);
+	});
+}
